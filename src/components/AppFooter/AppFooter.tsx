741474a09d7eb8a5b82c0c2736d7f568ea81b285
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-27 05:18:25 +0700

    Add AppFooter

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -12,70 +12,84 @@ const AppFooter: FC = () => {
         query: "(min-width: 1224px)"
     });
 
+    const link = ({ isActive }: { isActive: boolean }) =>
+        isActive ? `${styles.nav_link} ${styles.nav_link_active}` : `${styles.nav_link}`;
+
     const DesktopView: FC = () => {
         return (
             <>
                 <nav>
                     <div className={`${styles.container} ${styles.flex} ${styles.mb5}`}>
                         <div className={`${styles.width1n4}`}>
-                            <ul className={`${styles.list} ${styles.listOfmb4}`}>
-                                <li>
-                                    <NavLink to="/">About Us</NavLink>
+
+                            <ul className={`${styles.list}`}>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/about" className={`${styles.link}`}>About Us</NavLink>
                                 </li>
 
-                                <li>
-                                    <NavLink to="/">API</NavLink>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/api" className={`${styles.link}`}>API</NavLink>
                                 </li>
 
-                                <li>
-                                    <NavLink to="/">Contact Us</NavLink>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className={`${styles.width1n4}`}>
-                            <ul className={`${styles.list} ${styles.listOfmb4}`}>
-                                <li>
-                                    <NavLink to="/">Help/FAQ</NavLink>
-                                </li>
-
-                                <li>
-                                    <NavLink to="/">Support & Bugs</NavLink>
-                                </li>
-
-                                <li>
-                                    <NavLink to="/">Sitemap</NavLink>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/contact" className={`${styles.link}`}>Contact Us</NavLink>
                                 </li>
                             </ul>
                         </div>
 
                         <div className={`${styles.width1n4}`}>
-                            <ul className={`${styles.list} ${styles.listOfmb4}`}>
-                                <li>
-                                    <NavLink to="/">Privacy Policy</NavLink>
+                            <ul className={`${styles.list}`}>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/help" className={`${styles.link}`}>Help/FAQ</NavLink>
                                 </li>
 
-                                <li>
-                                    <NavLink to="/">Cookies Policy</NavLink>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/support" className={`${styles.link}`}>Support & Bugs</NavLink>
                                 </li>
 
-                                <li>
-                                    <NavLink to="/">Terms of Use</NavLink>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/sitemap" className={`${styles.link}`}>Sitemap</NavLink>
                                 </li>
                             </ul>
                         </div>
 
                         <div className={`${styles.width1n4}`}>
-                            <img src={LogoFooter} alt="" />
+                            <ul className={`${styles.list}`}>
+                                <li className={styles.mb4}>
+                                    <NavLink to="/privacy-policy" className={`${styles.link}`}>Privacy Policy</NavLink>
+                                </li>
+
+                                <li className={styles.mb4}>
+                                    <NavLink to="/cookies-policy" className={`${styles.link}`}>Cookies Policy</NavLink>
+                                </li>
+
+                                <li className={styles.mb4}>
+                                    <NavLink to="/terms" className={`${styles.link}`}>Terms of Use</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className={`${styles.width1n4} ${styles.flexCenterMiddle}`}>
+                            <NavLink to="/terms" className={`${styles.link}`}>
+                                <img src={LogoFooter} className={styles.w90} alt="" />
+                            </NavLink>
                         </div>
                     </div>
 
                     <div className={`${styles.container} ${styles.borderTop} ${styles.flex} ${styles.flexBetween} ${styles.pb4}`}>
                         <p className={`${styles.smallFontSize}`}>© 2023 Digiwalls Media, all rights reserved. All trademarks are property of their respective owners.</p>
                         <div>
-                            <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faFacebookF} />
-                            <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faTwitter} />
-                            <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faRss} />
+                            <NavLink to="/facebook" className={`${styles.link}`}>
+                                <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faFacebookF} />
+                            </NavLink>
+
+                            <NavLink to="/twitter" className={`${styles.link}`}>
+                                <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faTwitter} />
+                            </NavLink>
+
+                            <NavLink to="/rss" className={`${styles.link}`}>
+                                <FontAwesomeIcon className={`${styles.mr3} ${styles.awesomeIcon}`} icon={faRss} />
+                            </NavLink>
                         </div>
                     </div>
                 </nav >
