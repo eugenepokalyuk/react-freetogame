10543d078e5a87c35bcb8d1bd35948df6e5c5b40
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-26 16:26:31 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+      <HashRouter>
+        <App />
+      </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
