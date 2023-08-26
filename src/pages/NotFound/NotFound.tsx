de48dd89c5eec15708b6381beaa3897abfb540c1
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-27 05:19:23 +0700

    Update NotFound.tsx

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import styles from './NotFound.module.css';
 import { useMediaQuery } from "react-responsive";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const NotFound: FC = () => {
+    const params = useParams();
     const isDesktop = useMediaQuery({
         query: "(min-width: 1224px)"
     });
@@ -14,13 +15,16 @@ const NotFound: FC = () => {
                 <div className={styles.container}>
                     <div className={styles.content}>
                         <h1 className='text text_type_main-large mb-5 mt-10'>Oops! 404 Error</h1>
-                        <p className='text text_type_main-medium mb-4'>The page you requested does not exist</p>
+                        {params
+                            ? <p className='text text_type_main-medium mb-4'>The page {`${params['*']}`} you requested does not exist</p>
+                            : <p className='text text_type_main-medium mb-4'>The page you requested does not exist</p>
+                        }
                         <p className='text text_type_main-medium'>check the address or try <Link to='/' className={`${styles.link} ml-2`}>homepage</Link></p>
                     </div>
                 </div>
             </div>
-        )
-    }
+        );
+    };
 
     return (
         <main className={`${styles.main}`}>
