1bce238bfed72e0d7669755a3eb7e82dd4399e21
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-27 14:16:17 +0700

    Do Optional task
    При неудачном запросе должно быть три попытки повторного запроса

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,13 +2,30 @@ import axios from 'axios';
 
 const ApiUrlPath = 'https://cors-anywhere.herokuapp.com/https://www.freetogame.com/api';
 
-export const fetchGamesData = () => {
-    const endpoint = '/games';
-    let response = axios.get(`${ApiUrlPath}${endpoint}`);
-    return response;
-}
+// export const fetchGamesData = () => {
+//     const endpoint = '/games';
+//     let response = axios.get(`${ApiUrlPath}${endpoint}`);
+//     return response;
+// }
 export const fetchGameData = (id: number) => {
     const endpoint = `/game?${id}`;
     let response = axios.get(`${ApiUrlPath}${endpoint}`);
     return response;
+}
+
+export const fetchGamesData = async (maxRetries = 3) => {
+    const endpoint = '/games';
+    let retries = 0;
+
+    while (retries < maxRetries) {
+        try {
+            const response = await axios.get(`${ApiUrlPath}${endpoint}`);
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching games data:', error);
+            retries++;
+        }
+    }
+
+    throw new Error(`Failed to fetch games data after ${maxRetries} retries.`);
 }
