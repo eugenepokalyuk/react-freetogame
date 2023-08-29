9c3297cf85a1fd36128f8e4719a444dba46af4f4
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-08-30 00:27:04 +0700

    Create new Page & Route

---
+++ frontend/src/pages/InterfacePage/InterfacePage.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import styles from './InterfacePage.module.css';
+import FindGame from '../../components/FindGame/FindGame';
+
+const InterfacePage: FC = () => {
+    return (
+        <div className={styles.mt}>
+            <FindGame />
+        </div>
+    );
+};
+
+export default InterfacePage;
