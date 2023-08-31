a3811564dbffe88c5dcbafdf139c793a49d40957
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-09-01 00:15:49 +0700

    Update ModalOverlay.tsx

--- frontend/src/components/ModalOverlay/ModalOverlay.tsx
+++ frontend/src/components/ModalOverlay/ModalOverlay.tsx
@@ -2,16 +2,11 @@ import React, { FC } from 'react';
 import styles from './ModalOverlay.module.css';
 
 import { ModalOverlayProps } from '../../services/types';
-import { useMediaQuery } from 'react-responsive';
 
 const ModalOverlay: FC<ModalOverlayProps> = ({ onClose }) => {
     const handleOverlayClick = () => {
         onClose();
     };
-
-    const isDesktop = useMediaQuery({
-        query: "(min-width: 1224px)"
-    });
 
     return <div className={styles.overlay} onClick={handleOverlayClick}></div>;
 };
